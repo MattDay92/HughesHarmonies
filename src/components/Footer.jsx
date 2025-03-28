f54e0bc18dbe49362d9f1d405de17b2bfdf55155
Matt Day <mattday@matts-mac-mini.lan>
2025-03-28 13:53:29 -0400

    3/28 commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,19 +5,26 @@ import TikTok from '../components/photos/TikTok.png'
 
 export default function Footer() {
     return (
-        <footer>
-            <div>
-                Hughes Harmonies offers custom choral arrangements for all levels of ensembles. 
-                At competitively low prices, choose from previously arranged music or custom charts 
-                for your next competition or concert!
+        <footer className='py-5'>
+            <div className='d-flex w-75 justify-content-center align-items-center'>
+                <div className='footer-text'>
+                    Hughes Harmonies offers custom choral arrangements for all levels of ensembles.
+                    At competitively low prices, choose from previously arranged music or custom charts
+                    for your next competition or concert!
+                </div>
+                <div className='footer-contact-btn text-center'>
+                    <button className='btn btn-warning'>Contact Hughes Harmonies</button>
+                </div>
+                <div className='social-icons text-center'>
+                    <a href='https://www.facebook.com' target='_blank' className='btn facebook'><img src={Facebook} /></a>
+                    <a href='https://www.instagram.com' target='_blank' className='btn instagram'><img src={Instagram} /></a>
+                    <a href='https://www.tiktok.com' target='_blank' className='btn tiktok'><img src={TikTok} /></a>
+                </div>
             </div>
-            <div className='text-center'>
-                <button className='btn btn-warning'>Contact Hughes Harmonies</button>
-            </div>
-            <div className='social-icons text-center'>
-                <a href='https://www.facebook.com' target='_blank' className='btn facebook'><img src={Facebook} /></a>
-                <a href='https://www.instagram.com' target='_blank' className='btn instagram'><img src={Instagram} /></a>
-                <a href='https://www.tiktok.com' target='_blank' className='btn tiktok'><img src={TikTok} /></a>
+            <div className='d-flex w-75 justify-content-center align-items-center text-center my-2'>
+                © {new Date().getFullYear()}
+                <br />
+                Hughes Harmonies
             </div>
         </footer>
     )
